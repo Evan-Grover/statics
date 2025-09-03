462655c0a67d73fc3bff02c1c6515b39e7349fea
Evan-Grover <ehgrover123@gmail.com>
2025-09-03 11:47:11 -0400

    added test modules

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod quaternion{
     //A quaternion is the basic type used to represent all 3 dimensional vectors and rotations of those vectors in the simulator.
     //Because quaternions are confusing as hell, their functionality should be abstracted away as best as possible.
     //For my purposes, i is x, j is y, k is z
+    #[derive(PartialEq, Debug)]
     struct Quaternion{
         r: f64,
         i: f64,
@@ -30,9 +31,30 @@ mod quaternion{
                 k: (self.r * other.k) + (self.i * other.j) - (self.j * other.i) + (self.k * other.r) }
         }
 
+        //Gives you the inverse quaternion
+        fn invert(&self) -> Quaternion{
+            Quaternion { r: self.r, i: -self.i, j: -self.j, k: -self.k }
+        }
+
+        fn rotate(&self, rotation: &Quaternion) -> Quaternion{
+            rotation.mult(&self.mult(&rotation.invert()))
+        }   
+
         //Gets the absolut magnitude of the quarternion
         fn length(&self) -> f64{
             (self.r.powi(2) + self.i.powi(2) + self.j.powi(2) + self.k.powi(2)).sqrt()
+        }
+    }
+
+    #[cfg(test)]
+    mod tests{
+        use super::*;
+
+        #[test]
+        fn test_add(){
+            let numerand: Quaternion = Quaternion{ r: 1.0, i: 1.0, j: 1.0, k: 1.0};
+            let result: Quaternion = Quaternion { r: 2.0, i: 2.0, j: 2.0, k: 2.0 };
+            assert_eq!(numerand.add(&numerand), result)
         }
     }
 
